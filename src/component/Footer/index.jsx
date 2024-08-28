e5409f34a5c2ecff3c99d7c26a0a22885d35c28d
VanVodkaer <mail@vodkaer.com>
2024-08-28 23:17:57 +0800

    添加备案信息

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -3,7 +3,13 @@ import "./index.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>© MIAOHE.CAFE</p>
+      <p>
+        © MIAOHE.CAFE
+        <br />
+        <a href="https://beian.miit.gov.cn/" target="_blank">
+          鄂ICP备2024070655号-1
+        </a>
+      </p>
     </div>
   );
 }
